059a966c0514e92ae37e96452dcbbcade9708770
Joaco <lunaversex.official@gmail.com>
2025-07-09 23:49:17 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-const { default: makeWASocket, useSingleFileAuthState } = require('@whiskeysockets/baileys');
-const { state, saveState } = useSingleFileAuthState('./auth_info.json');
-const sock = makeWASocket({
-    auth: state
-});
-sock.ev.on('connection.update', (update) => {
-    const { connection, lastDisconnect } = update;
-    if(connection === 'close') {
-        if(lastDisconnect.error?.output?.statusCode !== 401) {
-            startSock();
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
+
+async function startSock() {
+    const { state, saveCreds } = await useMultiFileAuthState('./auth_info');
+    
+    const sock = makeWASocket({
+        auth: state
+    });
+    
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if(connection === 'close') {
+            const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
+            if(shouldReconnect) {
+                startSock();
+            }
+        } else if(connection === 'open') {
+            console.log('Conectado a WhatsApp');
         }
-    } else if(connection === 'open') {
-        console.log('Conectado a WhatsApp');
-    }
-});
-sock.ev.on('creds.update', saveState);
+    });
+    
+    sock.ev.on('creds.update', saveCreds);
+}
+
+startSock();
